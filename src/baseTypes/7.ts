79e678ab2dbf1f87d440c08af4743f2b4e251905
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2023-10-05 14:47:31 +0300

    finish

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,17 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum Weekday {
+  Mon,
+  Tue,
+  Wed,
+  Thu,
+  Fri,
+  Sat,
+  Sun,
+}
+
+function isWeekend(day: Weekday): boolean {
+  return day === Weekday.Sat || day === Weekday.Sun;
+}
--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,11 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<TKey, TValue> {
+  key: TKey;
+  value: TValue;
 }
+
+let data: KeyValuePair<string, string> = { key: "name", value: "Kate" };
 
 export {};
